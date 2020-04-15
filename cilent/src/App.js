ef39bba6abb06bb1232141636773d97f97d11245
saphirah.pociluyko <saphirah.j.pociluyko@usace.army.mil>
2020-04-15 14:58:48 -0500

    created a web page router for testing, added particles.js, still tinkering

--- cilent/src/App.js
+++ cilent/src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Blog from "./components/pages/Blog";
+import Contact from "./components/pages/Contact";
+import Particles from 'react-particles-js';
+import './App.css'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Router>
+    <div className="App"> 
+    <div>
+      <NavTabs />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/blog" component={Blog} />
+      <Route path="/contact" component={Contact} />
+      </div>
+      By Ankit Bansal 
+      <Particles 
+        params={{ 
+          particles: { 
+            number: { 
+              value: 200, 
+              density: { 
+                enable: true, 
+                value_area: 1000, 
+              } 
+            }, 
+          }, 
+        }} 
+      />   
     </div>
+  </Router>
+    
+    
   );
 }
 
