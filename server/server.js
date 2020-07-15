690a62f35ee6e42ca8b4521a19ef2c5740f101de
Will Breitkreutz <will.l.breitkreutz@usace.army.mil>
2020-07-15 13:49:12 -0500

    update server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,11 @@ const routes = require("./routes");
 const pkg = require("./package.json");
 const port = process.env.PORT;
 const cors = require("cors");
+
+// load our environment variables from the .env file
+const dotenv = require('dotenv');
+dotenv.config();
+
 app.use(cors());
 // add body parser so that we can get our data from the request
 app.use(bodyParser.json());
@@ -21,7 +26,7 @@ app.use((req, res, next) => {
 app.use("/api", routes);
 
 // serve the client application here
-// app.use("/", express.static(path.join(__dirname, "app")));
+app.use("/app", express.static(path.join(__dirname, "app")));
 
 // start the server up
 app.listen(port, () => console.log(`${pkg.name} listening on port ${port}!`));
